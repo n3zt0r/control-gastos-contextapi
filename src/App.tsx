a7ebfc32d2cae06d5704fb206bbe36cbee949224
Nestor Lopez <n3zt0r77@gmail.com>
2024-04-18 11:14:15 -0600

    Header and BudgetForm created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import BudgetForm from "./components/BudgetForm";
+
+export default function App() {
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        <BudgetForm />
+      </div>
+    </>
+  );
+}
