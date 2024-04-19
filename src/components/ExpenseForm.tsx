f6e03125ad9d08ca5c0677bab69ce0933240176d
Nestor Lopez <n3zt0r77@gmail.com>
2024-04-19 11:53:06 -0600

    Expense Form added

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,128 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import type { DraftExpense, Value } from "../types";
+import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import "react-calendar/dist/Calendar.css";
+import "react-date-picker/dist/DatePicker.css";
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
+
+export default function ExpenseForm() {
+  const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: "",
+    category: "",
+    date: new Date(),
+  });
+  const [error, setError] = useState("");
+  const { dispatch } = useBudget();
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    const isAmountField = ["amount"].includes(name);
+    setExpense({
+      ...expense,
+      [name]: isAmountField ? Number(value) : value,
+    });
+  };
+
+  const handleChangeDate = (value: Value) => {
+    setExpense({
+      ...expense,
+      date: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Validación
+    if (Object.values(expense).includes("")) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+
+    // Añadir nuevo gasto
+    dispatch({ type: "add-expense", payload: { expense } });
+
+    // Reiniciar el state
+    setExpense({ amount: 0, expenseName: "", category: "", date: new Date() });
+  };
+
+  return (
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+        Nuevo Gasto
+      </legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre Gasto:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          name="expenseName"
+          placeholder="Añade el nombre del gasto"
+          className="bg-slate-100 p-2"
+          value={expense.expenseName}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          placeholder="Añade la cantidad del gasto: Ej. 300"
+          className="bg-slate-100 p-2"
+          value={expense.amount}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="category" className="text-xl">
+          Categoria:
+        </label>
+        <select
+          id="category"
+          name="category"
+          className="bg-slate-100 p-2"
+          value={expense.category}
+          onChange={handleChange}
+        >
+          <option value="">--- Seleccione ---</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha Gasto:
+        </label>
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          value={expense.date}
+          onChange={handleChangeDate}
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value={"Registrar Gasto"}
+      />
+    </form>
+  );
+}
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,9 +1,29 @@
-export type BudgetActions = { type: "add-budget"; payload: { budget: number } };
+import { v4 as uuidv4 } from "uuid";
+import type { DraftExpense, Expense } from "../types";
 
-export type BudgetState = { budget: number };
+export type BudgetActions =
+  | { type: "add-budget"; payload: { budget: number } }
+  | { type: "show-modal" }
+  | { type: "close-modal" }
+  | { type: "add-expense"; payload: { expense: DraftExpense } };
+
+export type BudgetState = {
+  budget: number;
+  modal: boolean;
+  expenses: Expense[];
+};
 
 export const initialState: BudgetState = {
   budget: 0,
+  modal: false,
+  expenses: [],
+};
+
+const createExpense = (draftExpense: DraftExpense): Expense => {
+  return {
+    ...draftExpense,
+    id: uuidv4(),
+  };
 };
 
 export const budgetReducer = (
@@ -14,6 +34,30 @@ export const budgetReducer = (
     return {
       ...state,
       budget: action.payload.budget,
+    };
+  }
+
+  if (action.type === "show-modal") {
+    return {
+      ...state,
+      modal: true,
+    };
+  }
+
+  if (action.type === "close-modal") {
+    return {
+      ...state,
+      modal: false,
+    };
+  }
+
+  if (action.type === "add-expense") {
+    const expense = createExpense(action.payload.expense);
+
+    return {
+      ...state,
+      expenses: [...state.expenses, expense],
+      modal: false,
     };
   }
 
